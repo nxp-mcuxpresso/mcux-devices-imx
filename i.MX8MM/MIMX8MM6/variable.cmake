9f264f190b6a90bcc1f9e87b5001d18e0b8af76d
Jiafei Pan <Jiafei.Pan@nxp.com>
2025-08-10 00:40:15 +0800

    i.MX8MM: MIMX8MM6: add CA55 support
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- i.MX8MM/MIMX8MM6/variable.cmake
+++ i.MX8MM/MIMX8MM6/variable.cmake
@@ -1,5 +1,4 @@
-# Copyright 2024 NXP
-# All rights reserved.
+# Copyright 2024-2025 NXP
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -9,7 +8,11 @@ mcux_set_variable(device MIMX8MM6)
 mcux_set_variable(device_root devices)
 mcux_set_variable(soc_series i.MX8MM)
 mcux_set_variable(soc_periph periph)
-mcux_set_variable(core_id_suffix_name "_cm4")
-mcux_set_variable(multicore_foldername .)
+
+if (NOT DEFINED core_id)
+    message(FATAL_ERROR "Please specify core_id for multicore device.")
+endif()
+
+include(${CMAKE_CURRENT_LIST_DIR}/${core_id}/variable.cmake)
 
 #### Source record
