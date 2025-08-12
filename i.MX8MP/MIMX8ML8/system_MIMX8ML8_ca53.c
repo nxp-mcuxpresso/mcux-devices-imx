5993546a4b6e56c689264b91da36ff15f0feee4f
qizhang <jony.zhang@nxp.com>
2025-08-12 21:41:17 +0800

    [nxp toup] MIMX8ML8: port device header file from mcu-chipmodel
    Signed-off-by: qizhang <jony.zhang@nxp.com>

---
+++ i.MX8MP/MIMX8ML8/system_MIMX8ML8_ca53.c
@@ -0,0 +1,93 @@
+/*
+** ###################################################################
+**     Processors:          MIMX8ML8CVNKZ_ca53
+**                          MIMX8ML8DVNLZ_ca53
+**
+**     Compilers:           GNU C Compiler
+**                          IAR ANSI C/C++ Compiler for ARM
+**                          Keil ARM C/C++ Compiler
+**
+**     Reference manual:    IMX8MPRM, Rev.D, 12/2020
+**     Version:             rev. 6.0, 2024-10-29
+**     Build:               b250521
+**
+**     Abstract:
+**         Provides a system configuration function and a global variable that
+**         contains the system frequency. It configures the device and initializes
+**         the oscillator (PLL) that is part of the microcontroller device.
+**
+**     Copyright 2016 Freescale Semiconductor, Inc.
+**     Copyright 2016-2025 NXP
+**     SPDX-License-Identifier: BSD-3-Clause
+**
+**     http:                 www.nxp.com
+**     mail:                 support@nxp.com
+**
+**     Revisions:
+**     - rev. 1.0 (2019-10-11)
+**         Initial version.
+**     - rev. 2.0 (2020-02-21)
+**         Rev.B Header.
+**     - rev. 3.0 (2020-06-22)
+**         Rev.C Header.
+**     - rev. 4.0 (2020-11-16)
+**         Rev.D Header.
+**     - rev. 5.0 (2021-03-01)
+**         Rev.D Header Final.
+**     - rev. 6.0 (2024-10-29)
+**         Change the device header file from single flat file to multiple files based on peripherals,
+**         each peripheral with dedicated header file located in periphN folder.
+**
+** ###################################################################
+*/
+
+/*!
+ * @file MIMX8ML8_ca53
+ * @version 1.0
+ * @date 210525
+ * @brief Device specific configuration file for MIMX8ML8_ca53 (implementation file)
+ *
+ * Provides a system configuration function and a global variable that contains
+ * the system frequency. It configures the device and initializes the oscillator
+ * (PLL) that is part of the microcontroller device.
+ */
+
+#include <stdint.h>
+#include "fsl_device_registers.h"
+
+
+
+/* ----------------------------------------------------------------------------
+   -- Core clock
+   ---------------------------------------------------------------------------- */
+
+uint32_t SystemCoreClock = DEFAULT_SYSTEM_CLOCK;
+
+/* ----------------------------------------------------------------------------
+   -- SystemInit()
+   ---------------------------------------------------------------------------- */
+
+void SystemInit (void) {
+#if ((__FPU_PRESENT == 1) && (__FPU_USED == 1))
+  __ASM volatile("mov x0, #(3 << 20) \n\t"
+                 "msr cpacr_el1, x0");
+#endif /* ((__FPU_PRESENT == 1) && (__FPU_USED == 1)) */
+  SystemInitHook();
+
+  ARM_TIMER_GetFreq(&SystemCoreClock);
+}
+
+/* ----------------------------------------------------------------------------
+   -- SystemCoreClockUpdate()
+   ---------------------------------------------------------------------------- */
+
+void SystemCoreClockUpdate (void) {
+}
+
+/* ----------------------------------------------------------------------------
+   -- SystemInitHook()
+   ---------------------------------------------------------------------------- */
+
+__attribute__ ((weak)) void SystemInitHook (void) {
+  /* Void implementation of the weak function. */
+}
