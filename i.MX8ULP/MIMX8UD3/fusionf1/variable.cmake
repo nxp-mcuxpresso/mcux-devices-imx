a999a1c98ec7fd058bd24416a7e4ff330235b09d
Samuel Mudrik <samuel.mudrik@nxp.com>
2025-08-22 10:55:36 +0200

    Revert "[MCUX-82193][i.MX8ULP]Update xtensa toolchain to RJ 2025_5"
    This reverts commit f98647d03c1c69c5762b1987383b056a593dab91.

--- i.MX8ULP/MIMX8UD3/fusionf1/variable.cmake
+++ i.MX8ULP/MIMX8UD3/fusionf1/variable.cmake
@@ -1,4 +1,4 @@
-# Copyright 2024-2025 NXP
+# Copyright 2024 NXP
 # All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
@@ -8,7 +8,7 @@ mcux_set_variable(core_id_suffix_name _dsp0)
 mcux_set_variable(multicore_foldername fusionf1)
 
 if(NOT XTENSA_CORE)
-    mcux_set_variable(XTENSA_CORE nxp_imx8ulp_RJ25_5_newlib)
+    mcux_set_variable(XTENSA_CORE fusion_nxp02_dsp_prod)
 endif()
 
 mcux_set_variable(XTENSA_LSP_DEBUG ${SdkRootDirPath}/devices/i.MX/i.MX8ULP/MIMX8UD7/xtensa/gdbio)
