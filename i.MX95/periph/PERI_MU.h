a868c059cd2de640767aacbf3496f3378c8027be
Jiafei Pan <Jiafei.Pan@nxp.com>
2026-05-19 16:47:53 +0800

    i.MX95: periph: MU: add mu_core_boot_mode_t definition
    This patch is to fix compilation issue:
    mcuxsdk/mcuxsdk/drivers/mu1/fsl_mu.h:1073:38: error: unknown type name
    'mu_core_boot_mode_t'
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- i.MX95/periph/PERI_MU.h
+++ i.MX95/periph/PERI_MU.h
@@ -394,6 +394,16 @@
 #else
   #error Not supported compiler type
 #endif
+/*!
+ * @brief Core boot mode.
+ */
+typedef enum _mu_core_boot_mode
+{
+    kMU_CoreBootFromAddr0 = 0x00U, /*!< Boot from 0x00.      */
+    kMU_CoreBootFromFlash = 0x01U, /*!< Boot from Flash base. */
+    kMU_CoreBootFromItcm  = 0x02U, /*!< Boot from ITCM base. */
+} mu_core_boot_mode_t;
+
 
 /* ----------------------------------------------------------------------------
    -- MU Peripheral Access Layer
